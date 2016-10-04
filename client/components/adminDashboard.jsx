416b009357b22b1d601b26ab72d53e2781b469e2
weissna <weissna@rose-hulman.edu>
2016-10-04 17:17:24 -0400

    Added submit button. Working on connecting it to the backend

--- client/components/adminDashboard.jsx
+++ client/components/adminDashboard.jsx
@@ -1,36 +1,59 @@
 // This is a placeholder file for what happens when there is a successful login.
 // This page is shown when the login button is clicked and it's determined to be a successful login.
 
-const React = require('react');
+const React = require('react')
 
 const AdminDashboard = React.createClass({
-  getInitialState() {
+  getInitialState () {
+    // console.log('Temp');
     return {
       projectNames: ['Project1', 'Project2', 'Project3'],
-      projectIds: [1, 2, 3],
-    };
+      projectIds: [1, 2, 3]
+    }
   },
 
-  componentWillMount() {
+  componentWillMount () {
     // make api call to get projects and their ids. Set projectNames and projectIds
   },
 
-  createItem(item, index) {
-    const url = `/#/project?id=${this.state.projectIds[index]}`;
-    return <a href={url}><div className="project" key={index}>{item}</div></a>;
+  createItem (item, index) {
+    const url = `/#/project?id=${this.state.projectIds[index]}`
+    return <a href={url}><div className="project" key={index}>{item}</div></a>
   },
 
-  render() {
+  newProject (projectName) {
+    this.createItem(projectName, 4)
+  },
+
+  showForm () {
+    document.getElementById('projectForm').style.display = 'block'
+  },
+
+  hideForm () {
+    document.getElementById('projectForm').style.display = 'none',
+    document.getElementById('name').value = '',
+    document.getElementById('project').value = ''
+  },
+
+  render () {
     return (
       <div>
+        <input type="button" value="New Project" onClick={this.showForm} className="newProjButton"></input>
         <div className="dashTitle">Admin Dashboard</div>
+        <form className="projectForm" id="projectForm">
+          Name:
+          <input type="text" id="name" placeholder="Name"></input>
+          Project:
+          <input type="text" id="project" placeholder="Project"></input>
+          <button type="submit" className="projectInput" onClick={this.newProject(document.getElementById('project'))}>Submit</button>
+          <button type="submit" className="projectCancel" onClick={this.hideForm}>Cancel</button>
+        </form>
         <div className="dashboard">
           {this.state.projectNames.map(this.createItem)}
         </div>
       </div>
+      )
+  }
+})
 
-      );
-  },
-});
-
-module.exports = AdminDashboard;
+module.exports = AdminDashboard
