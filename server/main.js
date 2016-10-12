ab8c3fcf08217915f05ef151d96ef1d677438fd8
Josh Green <greenjm@rose-hulman.edu>
2016-10-12 05:00:46 +0100

    Fixed and updates users publications

--- server/main.js
+++ server/main.js
@@ -1,10 +1,17 @@
 import { Meteor } from 'meteor/meteor';
 import '../imports/startup/server/fixtures.js';
+
+// Methods
 import '../imports/api/login.js';
 import '../imports/api/users/methods.js';
-import '../imports/api/projects/projects.js';
 import '../imports/api/projects/methods.js';
+
+// Collections
+import '../imports/api/projects/projects.js';
+
+// Publications
 import '../imports/api/projects/server/publications.js';
+import '../imports/api/users/server/publications.js';
 
 Meteor.startup(() => {
   // code to run on server at startup
