998c550d9583fb48078f6ce83bafca81e73894b7
IanHavens <havensid@rose-hulman.edu>
2016-10-11 22:09:03 -0400

    api endpoint

--- imports/api/projects/server/publications.js
+++ imports/api/projects/server/publications.js
@@ -1,4 +1,5 @@
 import { Meteor } from 'meteor/meteor';
+import { check } from 'meteor/check';
 import '../projects.js';
 
 /* global Projects:true*/
@@ -19,4 +20,22 @@ Meteor.publish('projects', function projectsPublish() {
   const projects = Projects.find({ $or: [{
     managers: this.userId }, { employees: this.userId }] });
   return projects;
+});
+
+Meteor.publish('projectGet', function retrieveProject(projectId) {
+  check(projectId, String);
+  const currentUser = Meteor.users.findOne(this.userId);
+
+  if (currentUser == null || currentUser.profile == null) {
+    return null;
+  }
+
+  if (currentUser.profile.isAdmin) {
+    const project = Projects.find({ _id: projectId });
+    return project;
+  }
+
+  const project = Projects.find({ $and: [{ _id: projectId }, { $or: [{
+    managers: this.userId }, { employees: this.userId }] }] });
+  return project;
 });
