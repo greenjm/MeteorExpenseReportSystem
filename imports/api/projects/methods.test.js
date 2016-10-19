99420171c29e1a97305f64e61400f8f78d18d228
IanHavens <havensid@rose-hulman.edu>
2016-10-18 21:14:29 -0400

    Attempted mocha testing

--- imports/api/projects/methods.test.js
+++ imports/api/projects/methods.test.js
@@ -1,9 +1,50 @@
+import { Meteor } from 'meteor/meteor';
+import { Factory } from 'meteor/dburles:factory';
+// import StubCollections from 'meteor/hwillson:stub-collections';
+// import { PublicationCollector } from 'meteor/johanbrook:publication-collector';
+import { chai } from 'meteor/practicalmeteor:chai';
+// import { Accounts } from 'meteor/accounts-base';
+
+import { Projects } from './projects.js';
+// import './publications.js';
+// import '../../../startup/server/fixtures.js';
+
 /* eslint-env mocha */
 /* eslint-disable func-names, prefer-arrow-callback */
 /* global describe it:true */
 
-describe('Mocha test for api_projects_methods', function () {
-  it('should just work', function () {
-    console.log('I am at api_projects_methods!');
-  });
+if (Meteor.isServer) {
+  describe('Project methods', function () {
+    it('should create a project', function () {
+      console.log('I am at api_projects_methods!');
+    });
+
+    it('should add a manager to a project', function () {
+      console.log('adding manager');
+    });
+
+    it('should remove a manager from a project', function () {
+      console.log('removing manager');
+    });
+
+    it('should add an employee to a project', function () {
+      console.log('adding employee');
+    });
+
+    it('should remove an employee from a project', function () {
+      console.log('removing employee');
+    });
+
+    it('should activate a project', function () {
+      console.log('activating');
+    });
+
+    it('should deactivate a project', function () {
+      console.log('deactivate');
+    });
+
+    it('should edit the project name', function () {
+      console.log('changing names');
+    });
 });
+}
