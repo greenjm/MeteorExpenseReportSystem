0a6d37b22a7690d974d4504f3ca5b8972256f1ff
Josh Green <greenjm@rose-hulman.edu>
2017-01-11 17:52:51 +0000

    placeholder badge for notifications is in the header

--- imports/ui/components/header.jsx
+++ imports/ui/components/header.jsx
@@ -7,6 +7,8 @@ import IconButton from 'material-ui/IconButton';
 import IconMenu from 'material-ui/IconMenu';
 import MenuItem from 'material-ui/MenuItem';
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import Badge from 'material-ui/Badge';
+import NotificationsIcon from 'material-ui/svg-icons/social/notifications';
 
 const React = require('react');
 
@@ -48,18 +50,39 @@ const Header = React.createClass({
         <AppBar
           title="Expense Report"
           iconElementRight={
-            <IconMenu
-              iconButtonElement={
-                <IconButton><MoreVertIcon /></IconButton>
-              }
-              targetOrigin={{ horizontal: 'left', vertical: 'bottom' }}
-              anchorOrigin={{ horizontal: 'left', vertical: 'bottom' }}
-            >
-              {this.state.userDashLink}
-              {this.state.adminDashLink}
-              <MenuItem primaryText="Profile" />
-              <MenuItem primaryText="Sign out" onTouchTap={this.logout} />
-            </IconMenu>
+            <div>
+              <IconMenu
+                iconButtonElement={
+                  <Badge
+                    badgeContent={10}
+                    secondary
+                    badgeStyle={{ top: 22, right: 24, height: 18, width: 18 }}
+                    style={{ padding: 0 }}
+                  >
+                    <IconButton tooltip="Notifications">
+                      <NotificationsIcon />
+                    </IconButton>
+                  </Badge>
+                }
+                targetOrigin={{ horizontal: 'right', vertical: 'top' }}
+                anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
+              >
+                <MenuItem primaryText="View Notifications" />
+                <MenuItem primaryText="Dismiss all" />
+              </IconMenu>
+              <IconMenu
+                iconButtonElement={
+                  <IconButton><MoreVertIcon /></IconButton>
+                }
+                targetOrigin={{ horizontal: 'left', vertical: 'bottom' }}
+                anchorOrigin={{ horizontal: 'left', vertical: 'bottom' }}
+              >
+                {this.state.userDashLink}
+                {this.state.adminDashLink}
+                <MenuItem primaryText="Profile" />
+                <MenuItem primaryText="Sign out" onTouchTap={this.logout} />
+              </IconMenu>
+            </div>
           }
           showMenuIconButton={false}
         />
