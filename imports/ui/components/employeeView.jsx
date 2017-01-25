06406c2da12d886385a0fd1e51e089c85637cea8
Josh Green <greenjm@rose-hulman.edu>
2017-01-25 20:41:01 +0000

    fixed eslint issues

--- imports/ui/components/employeeView.jsx
+++ imports/ui/components/employeeView.jsx
@@ -163,10 +163,10 @@ const EmployeeView = React.createClass({
                 {this.state.projects.length > 0 ?
                   this.state.projects.map(this.createProjectRow) :
                   (
-                    <TableRow selectable={false}>
-                      <TableRowColumn>You do not belong to any projects.</TableRowColumn>
-                      <TableRowColumn />
-                    </TableRow>
+                  <TableRow selectable={false}>
+                    <TableRowColumn>You do not belong to any projects.</TableRowColumn>
+                    <TableRowColumn />
+                  </TableRow>
                   )
                 }
               </TableBody>
@@ -187,10 +187,10 @@ const EmployeeView = React.createClass({
                 {this.state.requests.length > 0 ?
                   this.state.requests.map(this.createRequestRow) :
                   (
-                    <TableRow selectable={false}>
-                      <TableRowColumn>You have not submitted any requests yet.</TableRowColumn>
-                      <TableRowColumn />
-                    </TableRow>
+                  <TableRow selectable={false}>
+                    <TableRowColumn>You have not submitted any requests yet.</TableRowColumn>
+                    <TableRowColumn />
+                  </TableRow>
                   )
                 }
               </TableBody>
@@ -206,10 +206,10 @@ const EmployeeView = React.createClass({
                 {this.state.requests.length > 0 ?
                   this.state.requests.map(this.createReportRow) :
                   (
-                    <TableRow selectable={false}>
-                      <TableRowColumn>You have not submitted any requests yet.</TableRowColumn>
-                      <TableRowColumn />
-                    </TableRow>
+                  <TableRow selectable={false}>
+                    <TableRowColumn>You have not submitted any requests yet.</TableRowColumn>
+                    <TableRowColumn />
+                  </TableRow>
                   )
                 }
                 <FlatButton
