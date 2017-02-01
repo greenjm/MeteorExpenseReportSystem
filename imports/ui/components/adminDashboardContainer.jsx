562354e67b3e9a8023ae487c3bd43d64ed463e65
Josh Green <greenjm@rose-hulman.edu>
2017-02-01 03:17:26 +0000

    Converted adminDashboard to Tab view

--- imports/ui/components/adminDashboardContainer.jsx
+++ imports/ui/components/adminDashboardContainer.jsx
@@ -2,26 +2,34 @@ import { Meteor } from 'meteor/meteor';
 import { createContainer } from 'meteor/react-meteor-data';
 import AdminDashboardPage from '../pages/adminDashboard.jsx';
 
-/* global Projects:true*/
+/* global Projects Reports:true*/
 /* eslint no-undef: "error"*/
 
 const AdminDashboardContainer = createContainer(() => {
   const user = Meteor.user();
   const profile = user && user.profile;
   const isAdmin = profile && profile.isAdmin;
+
   const projectSub = Meteor.subscribe('projects');
   const projectReady = projectSub.ready();
   const projects = projectReady && Projects.find().fetch();
+
   const userSub = Meteor.subscribe('users');
   const userReady = userSub.ready();
   const users = userReady && Meteor.users.find().fetch();
+
+  const reportSub = Meteor.subscribe('reports');
+  const reportReady = reportSub.ready();
+  const reports = reportReady && Reports.find().fetch();
   return {
     user: !!user || false,
     isAdmin,
     projectReady,
     userReady,
+    reportReady,
     projects: projects || [],
     users: users || [],
+    reports: reports || [],
   };
 }, AdminDashboardPage);
 
