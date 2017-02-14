045893e89d9f47578bff7621f83571d782345fc6
IanHavens <havensid@rose-hulman.edu>
2017-02-14 00:56:08 -0500

    color palette changes

--- imports/startup/client/theme.jsx
+++ imports/startup/client/theme.jsx
@@ -1,21 +1,21 @@
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import {
   grey900, grey500,
-  grey100, grey300, grey400,
+  grey100, grey300, grey200,
   white, darkBlack, fullBlack,
 } from 'material-ui/styles/colors';
 import { fade } from 'material-ui/utils/colorManipulator';
 
 export default function getTheme() {
   const palette = {
-    primary1Color: '#8c8b86',
+    primary1Color: grey200,
     primary2Color: grey900,
     primary3Color: grey100,
     accent1Color: '#ffae00',
-    accent2Color: grey400,
+    accent2Color: '#ffae00',
     accent3Color: grey500,
     textColor: darkBlack,
-    alternateTextColor: '#f9f9f9',
+    alternateTextColor: fullBlack,
     canvasColor: white,
     borderColor: grey300,
     disabledColor: fade(darkBlack, 0.3),
