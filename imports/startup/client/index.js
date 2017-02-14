5c04c5429da217c1ad07317780feabfdf79456fe
Josh Green <greenjm@rose-hulman.edu>
2017-02-14 07:38:34 +0000

    Small fix, reports imported on client

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -1,4 +1,5 @@
 // Client startup entry point. All client startup files should be imported here.
 import '../../api/receipts/receipts.js';
+import '../../api/reports/reports.js';
 import './routes.jsx';
 import '../../api/notifications/notifications.js';
